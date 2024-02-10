2a8dc7f4a8859613bd27e617fb572881585bb89b
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-02-10 14:22:08 +0900

    Initial Commit

---
+++ SampleWatchApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleWatchApp Watch App
+//
+//  Created by 近藤晴輝 on 2024/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleWatchApp Watch App/SampleWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWatchAppApp.swift
+//  SampleWatchApp Watch App
+//
+//  Created by 近藤晴輝 on 2024/02/10.
+//
+
+import SwiftUI
+
+@main
+struct SampleWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
